4b499d874fb5205b6545d315ee6f6348692727a3
012050 <beomjunchoe@naver.com>
2022-11-08 20:54:46 +0900

    VRAM 부족으로 실행 불가
    시스템 메모리를 GPU에 할당할 수 있다고 들었지만 추가 프로그램이 필요함
    코드에는 문제 없는걸로

--- var.py
+++ var.py
@@ -3,5 +3,4 @@ import keras_cv
 from tensorflow  import keras
 import matplotlib.pyplot as plt
 
-# model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
 model = keras_cv.models.StableDiffusion(img_width=320, img_height=320)
